d3f553c6d39cea316b5645cb0ff95860140c7a82
weilun <lun.wei@outlook.com>
2015-07-08 23:22:22 +0800

    add device function OK

--- admin/Home/Controller/AdminController.class.php
+++ admin/Home/Controller/AdminController.class.php
@@ -6,4 +6,29 @@ class AdminController extends BaseController {
 	public function admin(){
 		$this->display();
 	}
+
+	public function add_device(){
+		$device_sn = I('device_sn');
+
+		$data = array(
+				'sn' => $device_sn , 
+			);
+
+		$res = M('device')->add($data);
+
+		if( $res ) {
+			$this->success("设备添加成功");
+		}else {
+			$this->success("设备添加失败");
+		}
+	}
+
+
+	public function gps_info(){
+
+	}
+
+	public function alarm_info(){
+
+	}
 }
