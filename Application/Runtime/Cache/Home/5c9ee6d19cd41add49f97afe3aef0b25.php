eb7ec419701777dd8fabf4e0feb1b42a6f5f29c9
root <root@b1cli02.localdomain>
2015-07-10 17:15:17 +0800

    add jquey mobile support

--- Application/Runtime/Cache/Home/5c9ee6d19cd41add49f97afe3aef0b25.php
+++ Application/Runtime/Cache/Home/5c9ee6d19cd41add49f97afe3aef0b25.php
@@ -1,87 +1,41 @@
-<?php if (!defined('THINK_PATH')) exit();?><!DOCTYPE html>
-<!--[if lt IE 7 ]> <html lang="en" class="no-js ie6 lt8"> <![endif]-->
-<!--[if IE 7 ]>    <html lang="en" class="no-js ie7 lt8"> <![endif]-->
-<!--[if IE 8 ]>    <html lang="en" class="no-js ie8 lt8"> <![endif]-->
-<!--[if IE 9 ]>    <html lang="en" class="no-js ie9"> <![endif]-->
-<!--[if (gt IE 9)|!(IE)]><!--> <html lang="en" class="no-js"> <!--<![endif]-->
-    <head>
+<?php if (!defined('THINK_PATH')) exit();?><html lang="en" class="no-js">    
+ <head>
         <meta charset="UTF-8" />
         <!-- <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">  -->
-        <title>Login and Registration Form with HTML5 and CSS3</title>
-        <meta name="viewport" content="width=device-width, initial-scale=1.0"> 
-        <meta name="description" content="Login and Registration Form with HTML5 and CSS3" />
+        <title>Login</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" /> 
+        <meta name="description" content="Login" />
         <meta name="keywords" content="html5, css3, form, switch, animation, :target, pseudo-class" />
-        <meta name="author" content="Codrops" />
-        <link rel="shortcut icon" href="../favicon.ico"> 
-        <link rel="stylesheet" type="text/css" href="/athome/Public/css/demo.css" />
-        <link rel="stylesheet" type="text/css" href="/athome/Public/css/style2.css" />
-		<link rel="stylesheet" type="text/css" href="/athome/Public/css/animate-custom.css" />
-    </head>
+        <meta name="author" content="WeiLun" />
+	<link rel="stylesheet" href="http://code.jquery.com/mobile/latest/jquery.mobile.min.css" />  
+	<script src="http://code.jquery.com/jquery-1.7.1.min.js"></script>  
+	<script src="http://code.jquery.com/mobile/latest/jquery.mobile.min.js"></script>  
+ </head>
     <body>
-
-			<section>				
-                <div id="container_demo" >
-                    <!-- hidden anchor to stop jump http://www.css3create.com/Astuce-Empecher-le-scroll-avec-l-utilisation-de-target#wrap4  -->
-                    <a class="hiddenanchor" id="toregister"></a>
-                    <a class="hiddenanchor" id="tologin"></a>
-                    <div id="wrapper">
-                        <div id="login" class="animate form">
-                            <form  action="/athome/index.php/Home/User/signin" autocomplete="on" method="post"> 
-                                <h1>Log in</h1> 
-                                <p> 
-                                    <label for="username" class="uname" data-icon="u" > Your username </label>
-                                    <input id="username" name="name" required="required" type="text" placeholder="myusername or mymail@mail.com"/>
-                                </p>
-                                <p> 
-                                    <label for="password" class="youpasswd" data-icon="p"> Your password </label>
-                                    <input id="password" name="password" required="required" type="password" placeholder="eg. X8df!90EO" /> 
-                                </p>
-                                <p class="keeplogin"> 
-									<input type="checkbox" name="loginkeeping" id="loginkeeping" value="loginkeeping" /> 
-									<label for="loginkeeping">Keep me logged in</label>
-								</p>
-                                <p class="login button"> 
-                                    <input type="submit" value="Login" /> 
-								</p>
-                                <p class="change_link">
-									Not a member yet ?
-									<a href="#toregister" class="to_register">Join us</a>
-								</p>
-                            </form>
-                        </div>
-
-                        <div id="register" class="animate form">
-                            <form  action="/athome/index.php/Home/User/signup"  method="post" autocomplete="on"> 
-                                <h1> Sign up </h1> 
-                                <p> 
-                                    <label for="usernamesignup" class="uname" data-icon="u">Your username</label>
-                                    <input id="usernamesignup" name="usernamesignup" required="required" type="text" placeholder="mysuperusername690" />
-                                </p>
-                                <p> 
-                                    <label for="emailsignup" class="youmail" data-icon="e" > Your PhoneNum</label>
-                                    <input id="phonesignup" name="phonesignup" required="required" type="phone" placeholder="185xxxxxxx"/> 
-                                </p>
-                                <p> 
-                                    <label for="passwordsignup" class="youpasswd" data-icon="p">Your password </label>
-                                    <input id="passwordsignup" name="passwordsignup" required="required" type="password" placeholder="eg. X8df!90EO"/>
-                                </p>
-                                <p> 
-                                    <label for="passwordsignup_confirm" class="youpasswd" data-icon="p">Please confirm your password </label>
-                                    <input id="passwordsignup_confirm" name="passwordsignup_confirm" required="required" type="password" placeholder="eg. X8df!90EO"/>
-                                </p>
-                                <p class="signin button"> 
-									<input type="submit" value="Sign up"/> 
-								</p>
-                                <p class="change_link">  
-									Already a member ?
-									<a href="#tologin" class="to_register"> Go and log in </a>
-								</p>
-                            </form>
-                        </div>
-						
-                    </div>
-                </div>  
-            </section>
-        </div>
-    </body>
+      <div id="login">
+          <form  action="/athome/index.php/Home/User/signin" autocomplete="on" method="post">
+		<div data-role="header">
+              		<h1>Log in</h1> 
+	      	</div>
+   	      	<div data-role="filedcontain">
+                  	<label>Your username </label>
+                  	<input id="username" name="name" required="required" type="text" placeholder="myusername"/>
+		</div>
+   	      	<div data-role="filedcontain">
+                  	<label>Your password</label>
+                  	<input id="password" name="password" required="required" type="password" placeholder="eg. X8df!90EO" /> 
+		</div>
+			<label>
+      	        		<input type="checkbox" name="loginkeeping">Keep Login</input> 
+			</label>
+   	      	<div data-role="filedcontain">
+                 	<input type="submit" value="Login" /> 
+		</div>
+              	<p> 
+      			Not a member yet ?
+      			<a href="#toregister" class="to_register">Join us</a>
+      		</p>
+          </form>
+      </div>
+   </body>
 </html>
