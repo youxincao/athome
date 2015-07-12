f35be680e60801ffa9daa4846823dfc26378de04
weilun <lun.wei@outlook.com>
2015-07-12 09:24:58 +0800

    push weixin msg ok

--- admin/Common/Conf/config.php
+++ admin/Common/Conf/config.php
@@ -5,7 +5,7 @@ return array(
 	'DB_HOST'  => 'localhost' ,
 	'DB_NAME'  =>  'athome' ,
 	'DB_USER'  =>  'root' ,
-	'DB_PWD'   =>  'athome233' ,
+	'DB_PWD'   =>  'root' ,
 	'DB_PORT'  =>  '3306',
 	'DB_PREFIX' => 'db_'
 );
--- admin/Home/Controller/AdminController.class.php
+++ admin/Home/Controller/AdminController.class.php
@@ -1,7 +1,155 @@
 <?php
 namespace Home\Controller;
 use Think\Controller;
+
+/**
+ * 微信授权相关接口
+ * 
+ * @link http://www.phpddt.com
+ */
+ 
+class Wechat {
+    
+    //高级功能-》开发者模式-》获取
+    private $app_id = 'wx68c8c38c45de276d';
+    private $app_secret = '0aab8199e85964d14b8cadf844f5b04b';
+ 
+ 
+    /**
+     * 获取微信授权链接
+     * 
+     * @param string $redirect_uri 跳转地址
+     * @param mixed $state 参数
+     */
+    public function get_authorize_url($redirect_uri = '', $state = '')
+    {
+        $redirect_uri = urlencode($redirect_uri);
+        return "https://open.weixin.qq.com/connect/oauth2/authorize?appid={$this->app_id}&redirect_uri={$redirect_uri}&response_type=code&scope=snsapi_userinfo&state={$state}#wechat_redirect";
+    }
+    
+    /**
+     * 获取授权token
+     * 
+     * @param string $code 通过get_authorize_url获取到的code
+     */
+    public function get_open_id($code = '')
+    {
+        $token_url = "https://api.weixin.qq.com/sns/oauth2/access_token?appid={$this->app_id}&secret={$this->app_secret}&code={$code}&grant_type=authorization_code";
+        $token_data = $this->http($token_url);
+        
+        if($token_data[0] == 200)
+        {
+            return json_decode($token_data[1], TRUE);
+        }
+        
+        return FALSE;
+    }
+    
+    public function get_access_token(){
+		$token_url = "https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid={$this->app_id}&secret={$this->app_secret}";
+
+		$ch = curl_init();
+		//设置选项参数
+		curl_setopt($ch, CURLOPT_URL, $token_url);
+		curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE); 
+		curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, FALSE); 
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+
+		$token_data = curl_exec($ch);//执行
+		curl_close($ch);//释放curl句柄
+  //      $token_data = $this->http($token_url, 'GET');
+        p($token_data);
+        
+        if($token_data['errcode']  == null )
+        	return false;
+        return json_decode($token_data, TRUE);
+        
+    }
+
+    /**
+     * 获取授权后的微信用户信息
+     * 
+     * @param string $access_token
+     * @param string $open_id
+     */
+    public function get_user_info($access_token = '', $open_id = '')
+    {
+        if($access_token && $open_id)
+        {
+            $info_url = "https://api.weixin.qq.com/sns/userinfo?access_token={$access_token}&openid={$open_id}&lang=zh_CN";
+            $info_data = $this->http($info_url);
+            
+            if($info_data[0] == 200)
+            {
+                return json_decode($info_data[1], TRUE);
+            }
+        }
+        
+        return FALSE;
+    }
+    
+    public function http($url, $method, $postfields = null, $headers = array(), $debug = false)
+    {
+        $ci = curl_init();
+        /* Curl settings */
+        curl_setopt($ci, CURLOPT_HTTP_VERSION, CURL_HTTP_VERSION_1_1);
+        curl_setopt($ci, CURLOPT_CONNECTTIMEOUT, 30);
+        curl_setopt($ci, CURLOPT_TIMEOUT, 30);
+        curl_setopt($ci, CURLOPT_RETURNTRANSFER, true);
+ 
+        switch ($method) {
+            case 'POST':
+                curl_setopt($ci, CURLOPT_POST, true);
+                if (!empty($postfields)) {
+                    curl_setopt($ci, CURLOPT_POSTFIELDS, $postfields);
+                    $this->postdata = $postfields;
+                }
+                break;
+        }
+        curl_setopt($ci, CURLOPT_URL, $url);
+        curl_setopt($ci, CURLOPT_HTTPHEADER, $headers);
+        curl_setopt($ci, CURLINFO_HEADER_OUT, true);
+ 
+        $response = curl_exec($ci);
+        $http_code = curl_getinfo($ci, CURLINFO_HTTP_CODE);
+ 
+        if ($debug) {
+            echo "=====post data======\r\n";
+            var_dump($postfields);
+ 
+            echo '=====info=====' . "\r\n";
+            print_r(curl_getinfo($ci));
+ 
+            echo '=====$response=====' . "\r\n";
+            print_r($response);
+        }
+        curl_close($ci);
+        return array($http_code, $response);
+    }
+ 
+}
+
 class AdminController extends BaseController {
+
+	private function http_post_data($url, $data_string) {
+	    $curl = curl_init();
+	    curl_setopt($curl, CURLOPT_URL, $url); 
+	    curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, FALSE);
+	    curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, FALSE);
+	    curl_setopt($curl, CURLOPT_POST, 1);
+	    curl_setopt($curl, CURLOPT_POSTFIELDS, $data_string);
+	    curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+
+
+	    $result = curl_exec($curl);
+	    p($result);
+	    if (curl_errno($curl)) {
+	       return 'Errno'.curl_error($curl);
+	    }
+	    curl_close($curl);
+	    return $result;
+}
+
 
 	public function admin(){
 		$this->assign("device_list", M('device')->select());
@@ -130,6 +278,26 @@ class AdminController extends BaseController {
 		$data['type'] = $alarm_code;
 		$data[time] = time();
 		if( M('alarmrecord')->add($data) ) {
+			// 查看该sn是否有绑定的openid 
+				$openids = M('bind_openid_sn')->where(array('device_sn' => "111111"))->select();
+				$wechat = new Wechat();
+				$token = $wechat->get_access_token();
+	
+				// 发送微信消息
+				if( $openids && $token ){
+					foreach ($openids as $openid) {
+						$data['touser'] = $openid['openid'];
+						$data['msgtype'] ='text';
+						$data['text'] = array('content' => "Alarm Code:".$alarm_code);
+
+						p( json_encode($data));
+
+						$url = "https://api.weixin.qq.com/cgi-bin/message/custom/send?access_token=".$token['access_token'];
+						$this->http_post_data($url, json_encode($data));
+					}
+				}
+
+		
 			$res['status'] = 1;
 			$res['info'] = "报警信息添加成功";
 			$this->ajaxReturn($res, 'json');
